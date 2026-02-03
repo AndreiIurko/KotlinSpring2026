ecb179dddd519c5aa9119205aaec25fcaac9fc1e
Andrei Iurko <andrei.iurko@jetbrains.com>
2026-02-03 19:28:48 +0200

    Prepare for lecture 1

--- src/main/kotlin/org/jetbrains/kotlin/public/course/introduction/practice/task/Task.kt
+++ src/main/kotlin/org/jetbrains/kotlin/public/course/introduction/practice/task/Task.kt
@@ -1,6 +1,6 @@
 package org.jetbrains.kotlin.public.course.introduction.practice.task
 
-const val TO = 101
+const val TO = 100
 const val FROM = 10
 
 // Implement a simple console game where a random number between 10 and 100 is generated,
@@ -9,38 +9,13 @@ const val FROM = 10
 
 // Then log all user's answers into a string and print the log og the game.
 
-fun readAndPrepare(): Int?
+fun readNumber(): Int?
 {
-    println("Enter a number:")
-    return readlnOrNull()?.toIntOrNull()
+    TODO("Read the input and return it; return null if the input is not a number")
 }
 
 fun main() {
     val secretNumber = (FROM..TO).random()
-    val stringBuilder = StringBuilder()
 
-
-    do {
-        val number : Int? = readAndPrepare()
-        when (number) {
-            null -> {
-                "It's not a number".let {
-                    println(it)
-                    stringBuilder.append("$it${System.lineSeparator()}")
-                }
-                continue
-            }
-            in FROM..<secretNumber -> {
-                println("Entered number is too small")
-                stringBuilder.append("$number: Entered number is too small${System.lineSeparator()}")
-            }
-            in secretNumber + FROM..<TO -> {
-                println("Entered number is too big")
-                stringBuilder.append("$number: Entered number is too big${System.lineSeparator()}")
-            }
-        }
-        if (number !in FROM..TO) println("Number should be between 10 and 100")
-    } while (number != secretNumber)
-    println("You win!!!")
-    println(stringBuilder.toString())
+    TODO("Implement the game logic")
 }
